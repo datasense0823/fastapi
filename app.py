34be66d996dbf37b076026747443ce067607a243
datasense <datasense.learning@gmail.com>
2025-03-01 06:48:15 -0700

    live

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")  # Ensure the root path ("/") is defined
 def home():
-    return {"message": "Hello, FastAPI! 🚀 Your app is live!. I am learning something"}
+    return {"message": "Hello, FastAPI! 🚀 Your app is live!. I am learning something.tes2"}
 
 @app.get("/health")  # Optional health check endpoint
 def health_check():
